7d6c593eccd844c2d31665f8dd65f80429af1014
iizuka <iizuka.keishi@gmail.com>
2025-07-20 00:16:15 +0900

    fix: Add redirect from root to /jr page

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,15 @@ const withPWA = require('next-pwa')({
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/jr',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = withPWA(nextConfig);
